9fdf2a5b6280acb27988d7ceb6e24fc66e3c6fa8
Bastien Aracil <findbastien@gmail.com>
2023-04-20 20:11:23 +0200

    Add max-age in cache-control header.

--- net.femtoparsec.chess.server/src/main/java/net/femtoparsec/chess/server/ChessImageController.java
+++ net.femtoparsec.chess.server/src/main/java/net/femtoparsec/chess/server/ChessImageController.java
@@ -1,8 +1,11 @@
 package net.femtoparsec.chess.server;
 
+import io.micronaut.http.HttpHeaders;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.http.annotation.QueryValue;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -22,18 +25,19 @@ public class ChessImageController {
 
   private final @NonNull ServerBoardDrawer drawer;
 
-  @Get(value = "/v1/board.png", produces = MediaType.IMAGE_PNG)
-  public byte[] getBoardImage(@QueryValue(value = "size", defaultValue = "3") int size,
-                              @QueryValue(value = "lc",defaultValue = "D0D0D0") String lc,
-                              @QueryValue(value = "dc",defaultValue = "649B64") String dc,
-                              @QueryValue(value = "inv",defaultValue = "false") boolean inverted,
-                              @QueryValue(value = "wc",defaultValue = "false") boolean withCoordinates,
-                              @QueryValue(value = "line") String line) throws IOException {
+  @Get(value = "/v1/board/{line}", produces = MediaType.IMAGE_PNG)
+  public HttpResponse<byte[]> getBoardImage(
+      @PathVariable String line,
+      @QueryValue(value = "size", defaultValue = "3") int size,
+      @QueryValue(value = "lc", defaultValue = "D0D0D0") String lc,
+      @QueryValue(value = "dc", defaultValue = "649B64") String dc,
+      @QueryValue(value = "inv", defaultValue = "false") boolean inverted,
+      @QueryValue(value = "wc", defaultValue = "false") boolean withCoordinates) throws IOException {
     final var l = LineParser.parse(line.replace("_", " "), "en");
     final var image = drawer.draw(l,
         DrawParameter.builder()
-            .lightColor(Color.decode("#"+lc))
-            .darkColor(Color.decode("#"+dc))
+            .lightColor(Color.decode("#" + lc))
+            .darkColor(Color.decode("#" + dc))
             .size(Size.fromCode(size))
             .withCoordinates(withCoordinates)
             .inverted(inverted)
@@ -43,6 +47,9 @@ public class ChessImageController {
     try (var ob = new BufferedOutputStream(os)) {
       ImageIO.write(image, "PNG", ob);
     }
-    return os.toByteArray();
+    return HttpResponse.ok()
+        .contentType(MediaType.IMAGE_PNG_TYPE)
+        .body(os.toByteArray())
+        .header(HttpHeaders.CACHE_CONTROL, "max-age=86400");
   }
 }
